3c4bc144092eca3064293438cf70bc314b7036e5
satoshimeow <satoshi.meow@gmail.com>
2019-08-15 22:06:25 -0700

    Set the version number in gradle.

--- app/src/main/java/com/spockchain/wallet/ui/fragment/MineFragment.java
+++ app/src/main/java/com/spockchain/wallet/ui/fragment/MineFragment.java
@@ -1,23 +1,29 @@
 package com.spockchain.wallet.ui.fragment;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.View;
+import android.widget.TextView;
 
 import com.spockchain.wallet.R;
 import com.spockchain.wallet.base.BaseFragment;
 import com.spockchain.wallet.ui.activity.ContactsActivity;
-import com.spockchain.wallet.ui.activity.HelpActivity;
-import com.spockchain.wallet.ui.activity.MessageCenterActivity;
 import com.spockchain.wallet.ui.activity.NetSettingActivity;
-import com.spockchain.wallet.ui.activity.SystemSettingActivity;
 import com.spockchain.wallet.ui.activity.TransactionsActivity;
 import com.spockchain.wallet.ui.activity.WalletMangerActivity;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 
 public class MineFragment extends BaseFragment {
+
+
+    @BindView(R.id.tv_version_name)
+    TextView tvVersion;
+
     @Override
     public int getLayoutResId() {
         return R.layout.fragment_mine;
@@ -35,6 +41,13 @@ public class MineFragment extends BaseFragment {
 
     @Override
     public void configViews() {
+        try {
+            PackageInfo pInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
+            String version = pInfo.versionName;
+            tvVersion.append(version);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @OnClick({R.id.lly_wallet_manage, R.id.lly_trade_recode, R.id.lly_contacts, R.id.github_website, R.id.lly_system_setting})
